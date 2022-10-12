92a236ff787c02dc628cbfb890daea6b7bf66d02
p._.sc <p._.sc@218.38.137.27>
2022-10-13 03:40:31 +0900

    아이콘 성공!_! 아이콘 추가

---
+++ KoreanAir/src/main/webapp/JavaScript/js_Index.js
@@ -0,0 +1,16 @@
+const before = document.getElementById("before");
+const after = document.getElementById("after");
+
+
+
+function bf() {
+	if(  ) {
+		
+	}
+}
+
+function af() {
+	if(  ) {
+		
+	}
+}
