a529bfb85b07f88c2841f3b2bf5cdb9fb578c195
p._.sc <p._.sc@localhost>
2022-10-17 16:12:15 +0900

    대한항공 클론코딩 추가

--- KoreanAir/src/main/webapp/JavaScript/js_Index.js
+++ KoreanAir/src/main/webapp/JavaScript/js_Index.js
@@ -1,16 +1,12 @@
 const before = document.getElementById("before");
 const after = document.getElementById("after");
+let div_middle_top = document.getElementById("div_middle_top");
 
-
+after.addEventListener("click", af);
 
 function bf() {
-	if(  ) {
-		
-	}
+	
 }
 
 function af() {
-	if(  ) {
-		
-	}
 }
