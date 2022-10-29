67b2846a69e56caf1d7240076ab12cd601f60b48
PSungChul <sksh0000@naver.com>
2022-10-29 17:46:45 +0900

    배경사진 변경 성공

--- KoreanAir/src/main/webapp/JavaScript/js_Index.js
+++ KoreanAir/src/main/webapp/JavaScript/js_Index.js
@@ -1,12 +1,31 @@
 const before = document.getElementById("before");
 const after = document.getElementById("after");
-let div_middle_top = document.getElementById("div_middle_top");
+const div_middle_top = document.getElementById("div_middle_top");
+let middle = window.getComputedStyle(div_middle_top);
 
+before.addEventListener("click", bf);
 after.addEventListener("click", af);
 
 function bf() {
-	
+	if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_1.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_4.jpg')"
+	} else if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_2.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_1.jpg')"
+	} else if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_3.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_2.jpg')"
+	} else if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_4.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_3.jpg')"
+	}
 }
 
 function af() {
+	if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_1.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_2.jpg')"
+	} else if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_2.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_3.jpg')"
+	} else if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_3.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_4.jpg')"
+	} else if ( middle.backgroundImage === 'url("http://localhost:8080/KoreanAir/CSS/picture/Front_4.jpg")' ) {
+		div_middle_top.style.backgroundImage = "url('CSS/picture/Front_1.jpg')"
+	}
 }
